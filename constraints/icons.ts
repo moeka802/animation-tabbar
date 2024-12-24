391d7b0e29ea97b030c86d30a570f1d8c34ee552
moeka802 <moeka802.redapple@gmail.com>
2024-12-24 19:10:32 +0900

    add icons

--- constraints/icons.ts
+++ constraints/icons.ts
@@ -1,4 +1,14 @@
+// tsのエラーが出ている
 import home from "@/assets/icons/home.png";
-const icons = {
-  home: home,
+import add from "@/assets/icons/add.png";
+import heart from "@/assets/icons/heart.png";
+import user from "@/assets/icons/user.png";
+import search from "@/assets/icons/search.png";
+
+export default {
+  home,
+  add,
+  heart,
+  user,
+  search,
 };
