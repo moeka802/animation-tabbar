cf449c9bd6898d9dc0be5fda957418ceac197162
moeka802 <moeka802.redapple@gmail.com>
2024-12-24 23:51:04 +0900

    add Animated.View

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { Text, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Animated from "react-native-reanimated";
 
 type Props = {};
 
@@ -8,7 +9,15 @@ const Home = (props: Props) => {
   return (
     <SafeAreaView>
       <View className="px-5">
-        <Text>Home</Text>
+        <Text className="pt-10 pb-5">Home</Text>
+
+        <Animated.View
+          style={{
+            width: 100,
+            height: 100,
+            backgroundColor: "violet",
+          }}
+        ></Animated.View>
       </View>
     </SafeAreaView>
   );
