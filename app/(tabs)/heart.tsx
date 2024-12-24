1ca141ea3bac8400bbf5897fd1bb5a609e760236
moeka802 <moeka802.redapple@gmail.com>
2024-12-24 19:27:19 +0900

    add safeareaview

--- app/(tabs)/heart.tsx
+++ app/(tabs)/heart.tsx
@@ -1,13 +1,14 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 type Props = {};
 
 const Heart = (props: Props) => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>Heart</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
